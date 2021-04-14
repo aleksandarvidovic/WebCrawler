643c6d30a333aa63c912a75e1ab1ac16c9a33238
Aleksandar Vidovic <alvidovic@edu.aau.at>
2021-04-14 15:01:47 +0200

    Das ganze Projekt verbessert

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.print("Bitte geben Sie die URL der Website ein: ");
+
+        Scanner urlInput = new Scanner(System.in);
+
+        String websiteUrl = urlInput.nextLine();
+
+        urlInput.close();
+
+        WebCrawler webCrawler = new WebCrawler(websiteUrl);
+        webCrawler.crawlWebsite(2);
+    }
+}
--- src/main/java/WebCrawler.java
+++ src/main/java/WebCrawler.java
@@ -1,17 +1,83 @@
-import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class WebCrawler {
 
-    public static void main(String[] args) {
+    private ArrayList<String> urlsOnWebsite;
+    private String websiteUrl;
+    private static HashSet<String> visitedWebsites = new HashSet<>();
+    private Document document;
+    private String connectionMessage;
 
-        System.out.print("Bitte geben Sie die URL der Website ein: ");
+    WebCrawler(String websiteUrl){
+        this.websiteUrl = websiteUrl;
+        urlsOnWebsite = new ArrayList<>();
+        visitedWebsites.add(websiteUrl);
+    }
 
-        Scanner urlInput = new Scanner(System.in);
+    public boolean connectToWebsite(){
+        try {
+            document = Jsoup.connect(websiteUrl).get();
+        } catch (Exception e) {
+            connectionMessage = e.getMessage();
+        }
+        return document != null;
+    }
 
-        String websiteUrl = urlInput.nextLine();
+    public int countWordsOnWebsite() {
+        String[] wordsOnWebsite = Jsoup.parse(document.outerHtml()).text().split(" ");
+        return wordsOnWebsite.length;
+    }
 
-        urlInput.close();
+    public int countImagesOnWebsite(){
+        Elements images = document.getElementsByTag("img");
+        return images.size();
+    }
 
-        new Website(websiteUrl);
+    public int countVideosOnWebsite(){
+        Elements videos = document.getElementsByTag("video");
+        return videos.size();
+    }
+
+    public void printInformationAboutWebsite(){
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Die Website [" + websiteUrl + "] enthält:");
+        System.out.println(countWordsOnWebsite() + " Wörter, " + urlsOnWebsite.size() + " Links, " + countImagesOnWebsite() + " Bilder und " + countVideosOnWebsite() + " Videos.");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    public void scrapeURLSFromWebsite(){
+        Elements links = document.select("a[href]");
+        for (Element link : links) {
+            urlsOnWebsite.add(link.absUrl("href"));
+        }
+    }
+
+    public void crawlWebsite(int recursionDepth) {
+        if (recursionDepth < 0) return;
+        if (!connectToWebsite()) {
+            System.out.println("Verbindung zu dieser URL nicht möglich: " + websiteUrl + " -> " + connectionMessage + "\n");
+            return;
+        }
+        scrapeURLSFromWebsite();
+        printInformationAboutWebsite();
+        for (String link : urlsOnWebsite) {
+            if (!visitedWebsites.contains(link)) {
+                new WebCrawler(link).crawlWebsite(recursionDepth - 1);
+            }
+        }
+    }
+
+    public ArrayList<String> getUrlsOnWebsite(){
+        return urlsOnWebsite;
+    }
+
+    public static HashSet<String> getVisitedWebsites() {
+        return visitedWebsites;
     }
 }
